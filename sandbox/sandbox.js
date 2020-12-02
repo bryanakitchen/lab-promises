09e0975539ae104346f194653c5f47a2790bf8c7
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-02 16:41:53 -0500

    sandbox copy

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -3,4 +3,8 @@ const fsPromises = require('fs').promises;
 fsPromises.readFile('./README.md', 'utf-8' 
 ).then(console.log);
 
-fsPromises.writeFile('./READALSO.md', 'utf-8');
+fsPromises.writeFile('./READALSO.md', 'Another ReadMe');
+
+fsPromises.copyFile('./READALSO.md', './ReadCopy.md')
+    .then(() => console.log('READALSO.md was copied to ReadCopy.md'))
+    .catch(() => console.log('The file could not be copied.'));
